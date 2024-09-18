c5de6e136f9e64625665f328166df49a5390cb97
Yegor Yershov <yegor6741@gmail.com>
2024-09-18 20:59:56 +0700

    simpletap v0.1

---
+++ uniclick.py
@@ -0,0 +1,25 @@
+
+# from telegram import (
+# 	InlineKeyboardButton,
+# 	InlineKeyboardMarkup,
+# 	Update,
+# )
+
+# from telegram.ext import (
+# 	Application,
+# 	CommandHandler,
+# 	ContextTypes,
+# 	MessageHandler,
+# 	CallbackQueryHandler,
+# 	JobQueue,
+# 	filters,
+# )
+
+import asyncio
+from tg_client import init_client
+
+if __name__ == "__main__":
+	# test()
+	asyncio.run(init_client())
+
+
